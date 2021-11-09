84dbf39e7445ff7a9d89546edc3e4c38b9acffed
Haneul Ji <declan0518@gmail.com>
2021-11-09 11:47:02 +0900

    강의보면서 전체적으로 수정

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const bars = document.querySelector(".bars");
 const menus = document.querySelector(".nav-bar__menus");
 const sns = document.querySelector(".nav-bar__sns");
 
-const HIDDEN_CLASS = "hidden";
+const SHOW_CLASS = "active";
 
 function handleClick() {
-  menus.classList.toggle(HIDDEN_CLASS);
-  sns.classList.toggle(HIDDEN_CLASS);
+  menus.classList.toggle(SHOW_CLASS);
+  sns.classList.toggle(SHOW_CLASS);
 }
 
 bars.addEventListener("click", handleClick);
